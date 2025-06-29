69ba241279392d523f17ed5d3ccd7d630b2e055c
DYLANeay <dylan.eray6@gmail.com>
2025-06-29 13:31:31 +0200

    tout bête enfaite

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const isPalindrome = (str) => {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
+};
+
+console.log(isPalindrome('racecar'));
